47095d9f5def424c9524d44755d1e7fdc50d8bb8
alexxx2022 <105407484+alexxx2022@users.noreply.github.com>
2022-12-23 09:53:58 +0700

    Create Extensions.py

---
+++ telegram-bot/Extensions.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class Exchange:
+    @staticmethod
+    def get_price(base, quote, amount):
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Введите корректное название валюты {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Введите корректное название валюты {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Неправильный ввод данных')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = (json.loads(r.content)[keys[base]])*amount
+
+        return total_base
